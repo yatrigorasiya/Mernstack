e009de1b7f6e4aa200795e7834f18b1a8e0576b4
yatrigorasiya <yatrigorasiya2094@gmail.com>
2025-02-11 20:09:32 +0530

    update file

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -51,15 +51,7 @@ export const Register = ()=>{
                 navigate("/login")
             }else{
                 toast.error(res_data.extradetails ? res_data.extradetails : res_data.message,
-                    {
-                    position: "top-right",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: false,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "colored",}
+                  
                 )
             }
           
